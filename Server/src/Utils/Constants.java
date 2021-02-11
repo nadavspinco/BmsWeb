6a11e078949a41ee2eca314caeee994f9a7f44c9
Eitan Chen <eitanch@mta.ac.il>
2021-02-11 23:40:58 +0200

    start of menu page

--- Server/src/Utils/Constants.java
+++ Server/src/Utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String SystemManagment = "SystemManagment";
     public static final String USERID = "userid";
     public static final String Member_Page = "memberMenu.html";
-    public static final String Manager_Page = "managerMenu.html";
+    public static final String Manager_Page = "ManagerPage/managerMenu.html";
     public static final String Start_Page = "WebApp.html";
     public static final String Error = "error";
     public static final String Existed_Email = "error";
