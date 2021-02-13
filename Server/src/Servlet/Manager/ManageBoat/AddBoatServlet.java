8c9d85428d4e10542ddb9810cc79dd313997745c
Eitan Chen <eitanch@mta.ac.il>
2021-02-13 10:54:45 +0200

    done add member and add boat

---
+++ Server/src/Servlet/Manager/ManageBoat/AddBoatServlet.java
@@ -0,0 +1,68 @@
+package Servlet.Manager.ManageBoat;
+import Enum.BoatTypeEnum;
+import Objects.SystemManagement;
+import Utils.Constants;
+import Utils.ServletUtils;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "AddBoatServlet", urlPatterns = "/addBoat")
+public class AddBoatServlet extends HttpServlet {
+    private Gson gson = new Gson();
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AddBoatServlet(req, resp);
+    }
+
+    public void AddBoatServlet(HttpServletRequest req, HttpServletResponse resp) {
+        try (PrintWriter out = resp.getWriter()) {
+            SystemManagement systemManagement = ServletUtils.getSystemManagment(getServletContext());
+            HttpSession session = req.getSession();
+            if (session == null) {
+                out.print(Constants.Error);// TODO MAKE TO REDIRECT TO HOME PAGE
+                return;
+            }
+
+            BufferedReader reader = req.getReader();
+            String gsonString = reader.lines().collect(Collectors.joining());
+            BoatArgs boatArgs = gson.fromJson(gsonString, BoatArgs.class);
+            String name = boatArgs.name;
+            String serial = boatArgs.serial;
+            boolean isCoastal = boatArgs.coastal;
+            boolean isWide = boatArgs.wide;
+            BoatTypeEnum boatType = BoatTypeEnum.convertFromInt(boatArgs.boatType);
+
+            if(systemManagement.isBoatExistBySerial(serial)){
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                out.print(Constants.Existed_Serial);// TODO MAKE TO REDIRECT TO HOME PAGE
+                return;
+            }
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+            systemManagement.addBoat(name, boatType, isCoastal, isWide, serial);
+            String redirectUrlPage = Constants.Manager_Page;
+            out.print(redirectUrlPage);
+        }
+        catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    static class BoatArgs{
+        private String name;
+        private String serial;
+        private boolean coastal;
+        private boolean wide;
+        private int boatType;
+    }
+}
---
+++ Server/src/Servlet/Manager/ManageMember/AddMemberServlet.java
@@ -0,0 +1,75 @@
+package Servlet.Manager.ManageMember;
+import Enum.LevelEnum;
+import Objects.SystemManagement;
+import Utils.Constants;
+import Utils.ServletUtils;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "AddMemberServlet", urlPatterns = "/addMember")
+public class AddMemberServlet extends HttpServlet {
+    private Gson gson = new Gson();
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AddMember(req, resp);
+    }
+
+    public void AddMember(HttpServletRequest req, HttpServletResponse resp) {
+        try (PrintWriter out = resp.getWriter()) {
+            SystemManagement systemManagement = ServletUtils.getSystemManagment(getServletContext());
+            HttpSession session = req.getSession();
+            if (session == null) {
+                out.print(Constants.Error);// TODO MAKE TO REDIRECT TO HOME PAGE
+                return;
+            }
+
+            BufferedReader reader = req.getReader();
+            String gsonString = reader.lines().collect(Collectors.joining());
+            MemberArgs member = gson.fromJson(gsonString, MemberArgs.class);
+            LevelEnum level = LevelEnum.convertFromInt(member.level);
+
+            if(systemManagement.isMemberExistBySerial(member.serial)){
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                out.print(Constants.Existed_Serial);// TODO MAKE TO REDIRECT TO HOME PAGE
+                return;
+            }
+
+            if(systemManagement.isEmailAlreadyExist(member.email)){
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                out.print(Constants.Existed_Email);// TODO MAKE TO REDIRECT TO HOME PAGE
+                return;
+            }
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+            systemManagement.addMember(member.name,member.phone,member.email,member.password,member.age,
+                    member.comment, level, member.isManager, member.serial);
+            String redirectUrlPage = Constants.Manager_Page;
+            out.print(redirectUrlPage);
+        }
+        catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    static class MemberArgs{
+        private String email;
+        private String password;
+        private String name;
+        private String serial;
+        private String comment;
+        private String phone;
+        private int age;
+        private int level;
+        private boolean isManager;
+    }
+}
