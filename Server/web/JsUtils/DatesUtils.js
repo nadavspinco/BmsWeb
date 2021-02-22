5f68be9fff595e9a0b3553651af8c7091aea5adf
Eitan Chen <eitanch@mta.ac.il>
2021-02-22 18:53:06 +0200

    finish show + remove registration

--- Server/web/JsUtils/DatesUtils.js
+++ Server/web/JsUtils/DatesUtils.js
@@ -1,3 +1,11 @@
+function localDateTimeToString(localDateTime){
+ return LocalDateToString(localDateTime.date)+ ' ' + localTimeToString(localDateTime.time)
+}
+
+function localTimeToString(localTime){
+ return localTime.hour + ':' +localTime.minute
+}
+
 function LocalDateToString(localDate){
- return  localDate.day +'/' +localDate.month+'/' + localDate. year
+ return  localDate.day +'/' +localDate.month+'/' + localDate.year
 }
